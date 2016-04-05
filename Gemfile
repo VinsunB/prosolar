c25df88cf0a0aaf3cf774553b967300aceb2e021
VinsunB <vinsuncoding@gmail.com>
2016-04-04 21:43:38 -0400

    heroku setup

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+ruby '1.9.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
@@ -14,8 +14,9 @@ group :production do
  
 gem 'pg'
 gem 'rails_12factor'
-end
 gem 'puma'
+gem 'foreman', '~> 0.78.0'
+end
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,18 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2) 
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count<br/>
-preload_app!<br/>
-rackup DefaultRackup<br/>
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+
+preload_app!
+
+rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'<br/>
+environment ENV['RACK_ENV'] || 'development'
+
 on_worker_boot do
-    ActiveRecord::Base.establish_connection
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+             Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
